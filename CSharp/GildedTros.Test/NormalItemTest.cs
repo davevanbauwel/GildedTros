06ddb69b9d25c87118025649605f7d993a828b9f
Dave Van Bauwel <Dave.VanBauwel@wisetechglobal.com>
2026-06-02 11:56:58 +0200

    Add working UT code for NormalItemTest based on current logic

--- CSharp/GildedTros.Test/NormalItemTest.cs
+++ CSharp/GildedTros.Test/NormalItemTest.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using GildedTros.App;
+using Xunit;
 
 namespace GildedTros.Test
 {
@@ -7,19 +8,46 @@ namespace GildedTros.Test
 		[Fact]
 		public void TestQuality_DegradeByOneEachDay()
 		{
+			var items = new List<Item>
+			{
+				new Item { Name = "Ring of Cleansening Code", SellIn = 10, Quality = 20 }
+			};
+			var app = new InventoryManager(items);
 
+			app.UpdateQuality();
+
+			Assert.Equal(19, items[0].Quality);
+			Assert.Equal(9, items[0].SellIn);
 		}
 
 		[Fact]
 		public void TestQuality_DegradeTwiceAsFastAfterSellByDay()
 		{
+			var items = new List<Item>
+			{
+				new Item { Name = "Ring of Cleansening Code", SellIn = 0, Quality = 20 }
+			};
+			var app = new InventoryManager(items);
 
+			app.UpdateQuality();
+
+			Assert.Equal(18, items[0].Quality);
+			Assert.Equal(-1, items[0].SellIn);
 		}
 
 		[Fact]
 		public void TestQuality_NeverNegative()
 		{
+			var items = new List<Item>
+			{
+				new Item { Name = "Ring of Cleansening Code", SellIn = 10, Quality = 0 }
+			};
+			var app = new InventoryManager(items);
 
+			app.UpdateQuality();
+
+			Assert.Equal(0, items[0].Quality);
+			Assert.Equal(9, items[0].SellIn);
 		}
 	}
 }
